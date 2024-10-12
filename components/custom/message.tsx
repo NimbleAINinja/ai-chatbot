057cba950d7d69bc4e41793a01cfd6f058e64289
Graham V <hydrofire@gmail.com>
2024-10-12 18:09:02 -0400

    Green text for assistant messages

--- components/custom/message.tsx
+++ components/custom/message.tsx
@@ -26,13 +26,13 @@ export const Message = ({
       initial={{ y: 5, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
     >
-      <div className="size-[24px] flex flex-col justify-center items-center shrink-0 text-zinc-400">
+      <div className="size-[24px] flex flex-col justify-center items-center shrink-0 text-zinc-400 ">
         {role === "assistant" ? <BotIcon /> : <UserIcon />}
       </div>
 
       <div className="flex flex-col gap-2 w-full">
         {content && (
-          <div className="text-zinc-800 dark:text-zinc-300 flex flex-col gap-4">
+          <div className={`${role === "assistant" ? "text-green-800 dark:text-green-400 [text-shadow:_0_0_4px_#0fa]" : "text-zinc-800 dark:text-zinc-300"} flex flex-col gap-4`}>
             <Markdown>{content as string}</Markdown>
           </div>
         )}
